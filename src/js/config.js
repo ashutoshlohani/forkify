22aa3b0ca5d11de3635cc771911810068510754e
ashutoshlohani <ashulohani97@outlook.com>
2022-01-16 20:08:54 +0530

    first commit!!!

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '926af89b-92fe-459a-9251-df07ffa08417';
+export const MODAL_CLOSE_SEC = 2.5;
